78910369c7dad3ae875b841ce70cf80264e323fa
mohanasundaram-s <61871852+mohanasundaram-s@users.noreply.github.com>
2020-03-10 14:55:12 +0530

    Add files via upload

---
+++ reverseOfNumbers.java
@@ -0,0 +1,19 @@
+//write a program to find the reverse of a number. 
+import java.util.Scanner;
+class reverseOfNumbers{
+    public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+        int userInput,reverse=0;
+        
+        System.out.print("Enter the number:"); 
+        userInput = input.nextInt();
+        int value = userInput;
+
+        while(value != 0){
+            int singleDigital = value % 10;
+            reverse = reverse*10+singleDigital;
+            value/=10;
+        }
+        System.out.println("Reverse number :"+reverse);
+    }
+}
