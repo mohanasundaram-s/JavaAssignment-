10e3acc53e8076f03963713f29f0dd1658cb278b
mohanasundaram-s <61871852+mohanasundaram-s@users.noreply.github.com>
2020-03-10 14:53:55 +0530

    Add files via upload

---
+++ ArithmeticProgressions.java
@@ -0,0 +1,32 @@
+//Write a program to print Arithmetic Progression series 
+import java.util.Scanner;
+
+class ArithmeticProgression{
+    public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+
+            int StartingNo = 0;
+            int NoOfElement = 0;
+            int Difference = 0;
+            int term = 0;
+
+            System.out.print("Enter the  starting number:"); 
+            StartingNo = input.nextInt();
+
+            System.out.print("Enter the Difference:"); 
+            Difference = input.nextInt();
+
+            System.out.print("Enter the NoOfElement:"); 
+            NoOfElement = input.nextInt();
+
+            System.out.println("");
+            System.out.println("The Arithmetic Progression is as follows :");
+
+            for(int i = 0; i < NoOfElement; i++){
+                term = StartingNo + i * Difference;
+                System.out.print(term+",");
+            }
+            
+            System.out.println("...");
+    }
+}
